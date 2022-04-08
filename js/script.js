002c52ab55e101fff40b1bfa501515ddb19d2894
Silvia-Du <silviadu@hotmail.it>
2022-04-08 17:27:40 +0200

    M3 - assegnazione stili differenti alle eccezioni dei multipli.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
 /*
-a- scrivere un programma che stampi in console i numeri da 1 a 100;
-b M1- per i multipli di 3 stampare Fizz al posto del numero/ Buzz per i multipli di 5 /     entrambi i termini per i condivisi;
-c M2- dato un contenitore nel DOM appendi al suo interno un elemento html;
-d M3- stile differente per i multipli di 3, 5 e condivisi;
-
 Flusso di lavoro:
 1. creare ciclo for con i da 1 a 100;
 2. assegnare valori diversi ai multipli nel ciclo for.
@@ -11,6 +6,7 @@ Flusso di lavoro:
    creo elemento da aggiungere / con una classe dedicata
    lo aggiungo al contenitore
    creo le eccezioni per il contenuto
+4. Assegnazione stile differente per multipli != e condivisi.
 */
 
 const limit =100;
@@ -33,19 +29,22 @@ for(let i = 1; i <=limit; i++){
     console.log(i);
    }
 
-   //3.
+   //3. 4.
    const boxEr =document.createElement('div');
    boxEr.className = 'box';
    conTainer.append(boxEr);
 
    if((i % 3 === 0) && (i % 5 === 0)){
     boxEr.append('FizzBuzz');
-    
+    boxEr.classList.add('fizz-buzz-box')
+
    } else if(i % 3 === 0){
     boxEr.append('Fizz');
+    boxEr.classList.add('fizz-box')
 
    } else if(i % 5 === 0){
     boxEr.append('Buzz');
+    boxEr.classList.add('buzz-box')
 
    } else {
     boxEr.append(i);

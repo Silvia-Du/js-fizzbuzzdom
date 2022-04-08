63a9e900f5eac5dba20434f8f66ac75cf8ed0973
Silvia-Du <silviadu@hotmail.it>
2022-04-08 15:06:44 +0200

    scritto ciclo for per stampare  da 1 a 100

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+a- scrivere un programma che stampi in console i numeri da 1 a 100;
+b M1- per i multipli di 3 stampare Fizz al posto del numero/ Buzz per i multipli di 5 /     entrambi i termini per i condivisi;
+c M2- dato un contenitore nel DOM appendi al suo interno un elemento html;
+d M3- stile differente per i multipli di 3, 5 e condivisi;
+
+Flusso di lavoro:
+1. creare ciclo for con i da 1 a 100;
+
+*/
+
+const limit =100;
+
+for(let i =1; i <=limit; i++){
+  console.log(i);
+}
